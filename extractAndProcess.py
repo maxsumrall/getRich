bb4ae66a75b9599bf52e2ccf028f9e683de13a2d
Max Sumrall <jmsumrall@gmail.com>
2014-12-03 18:14:51 +0100

    added mongo parts

--- extractAndProcess.py
+++ extractAndProcess.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 
-import os, tarfile, bz2, json, subprocess
+import os, tarfile, bz2, json, subprocess, pymongo
 
 tempdir = "/root/tempdir/"
 archive = "archive-09.tar"
 bz = []
 tar = tarfile.TarFile("archive-09.tar")
+
+client = pymongo.MongoClient()
+db = client.test_database
+tweets = db.tweets
+
 for member in tar:
 	tar.extractall(path=tempdir, members=[member])
 	if(member.isfile()):
@@ -13,8 +18,9 @@ for member in tar:
 		for line in bz:
 			data = json.loads(line)
 			if ("text" in data.keys()) and ("en" in data["lang"]):
-				print data["text"]
+				#print data["text"]
+				tweets.insert({"created_at": data["created_at"], "text": data["text"]})
 		subprocess.check_output("rm " + tempdir+member.name,shell=True)
-		if "n" in raw_input():
-			exit()
+		#if "n" in raw_input():
+		#	exit()
 
