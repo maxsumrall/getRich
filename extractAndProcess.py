e6f5e2474d5c8c43411d996b1981e53a69c8b4b3
Max Sumrall <jmsumrall@gmail.com>
2014-12-05 15:10:38 +0100

    Update extractAndProcess.py

--- extractAndProcess.py
+++ extractAndProcess.py
@@ -12,15 +12,14 @@ db = client.test_database
 tweets = db.tweets
 
 for member in tar:
-	tar.extractall(path=tempdir, members=[member])
-	if(member.isfile()):
-		bz = bz2.BZ2File(tempdir+member.name)
-		for line in bz:
-			data = json.loads(line)
-			if ("text" in data.keys()) and ("en" in data["lang"]):
-				#print data["text"]
-				tweets.insert({"created_at": data["created_at"], "text": data["text"]})
-		subprocess.check_output("rm " + tempdir+member.name,shell=True)
-		#if "n" in raw_input():
-		#	exit()
-
+        tar.extractall(path=tempdir, members=[member])
+        if(member.isfile()):
+                bz = bz2.BZ2File(tempdir+member.name)
+                for line in bz:
+                        data = json.loads(line)
+                        if ("text" in data.keys()) and ("en" in data["lang"]):
+                                #print data["text"]
+                                tweets.insert({"created_at": data["created_at"], "text": data["text"]})
+                subprocess.check_output("rm " + tempdir+member.name,shell=True)
+                #if "n" in raw_input():
+                #       exit()
