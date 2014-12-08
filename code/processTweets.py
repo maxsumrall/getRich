261f2a5216fcfb97b63bc50d5d74ac19a27b6b78
Max Sumrall <jmsumrall@gmail.com>
2014-12-08 19:39:43 +0100

    hash compiled regex, might be faster

--- code/processTweets.py
+++ code/processTweets.py
@@ -40,8 +40,8 @@ def calculateMoodsSentiment():
     currDay = 0
     countToday = 1.0
     days = {}
-    for tweet in tweets.find():
-        if True:
+    for tweet in tweets.find()[:1000]:
+        if len(set(tweet["text"].lower().split()) & emotional_words_filter_set) > 0:
             date = datetime.strptime(tweet["created_at"], '%a %b %d %H:%M:%S +0000 %Y')
             key = str(date.month) + "/" + str(date.day)
             tweetMoods = plutchik.executeTweet(tweet["text"])
@@ -65,7 +65,7 @@ def calculateMoodsSentiment():
               + "," + str(sentiments[7] / count)
 
     #normalize
-    avg = []
+    """avg = []
     for key in days.keys():
         sentiments, count = days[key]
         avg.append((key, [(sentiments[0] / count), (sentiments[1] / count), (sentiments[2] / count), (sentiments[3] / count), (sentiments[4] / count), (sentiments[5] / count), (sentiments[6] / count), (sentiments[7] / count)]))
@@ -81,7 +81,7 @@ def calculateMoodsSentiment():
     for i in range(len(avg)):
         for j in range(len(avg[i][1])):
             avg[i][1][j] = (avg[i][1][j] - min) / (max - min)
-    print "day,anger,anticipation,disgust,fear,joy,sadness,surprise,trust"
+    print "day,joy,trust,fear,surprise,sadness,disgust,anger,anticipation"
     for day in avg:
         print day[0] \
               + "," + str(day[1][0]) \
@@ -93,7 +93,7 @@ def calculateMoodsSentiment():
               + "," + str(day[1][6]) \
               + "," + str(day[1][7])
 
-
+    """
 
 def countEmotWords():
     count = 0
