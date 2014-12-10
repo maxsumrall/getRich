2474dcc00daa964ca7f24bc6d3009ad3fdbfc820
Giedo Mak <giedomak@gmail.com>
2014-12-10 15:35:24 +0100

    catch interrupt and kill thread

--- code/processTweets.py
+++ code/processTweets.py
@@ -17,9 +17,10 @@ tweets = db.tweets
 tweetNumber = 0
 finish = False
 
+days = {}
+
 
 def printit():
-    print finish
     if not finish:
         threading.Timer(5.0, printit).start()
     sys.stdout.write("\r%f%%" % (tweetNumber/float(tweets.count()))*100)
@@ -48,81 +49,86 @@ http_regex = re.compile('(http://)', re.IGNORECASE)
 
 
 def calculateMoodsSentiment():
-    global tweetNumber
-    days = {}
-    print "Number of tweets: " + str(tweets.count())
-    for tweet in tweets.find():
-        # for printing progress
-        tweetNumber += 1
+    try:
+        global tweetNumber
+        global days
+        print "Number of tweets: " + str(tweets.count())
+        for tweet in tweets.find():
+            # for printing progress
+            tweetNumber += 1
 
-        # only process tweets that don't have http:// in there
-        if len(http_regex.findall(tweet["text"])) is 0:
+            # only process tweets that don't have http:// in there
+            if len(http_regex.findall(tweet["text"])) is 0:
 
-            date = datetime.datetime.strptime(tweet["created_at"], '%a %b %d %H:%M:%S +0000 %Y')
-            key = str(date.month) + "/" + str(date.day)
-            tweetMoods = plutchik.executeTweet(tweet["text"])
-            if key in days.keys():
-                days[key][1] += 1
-                for i in range(len(tweetMoods)):
-                    days[key][0][i] += tweetMoods[i]
-            else:
-                days[key] = [tweetMoods, 1.0]
+                date = datetime.datetime.strptime(tweet["created_at"], '%a %b %d %H:%M:%S +0000 %Y')
+                key = str(date.month) + "/" + str(date.day)
+                tweetMoods = plutchik.executeTweet(tweet["text"])
+                if key in days.keys():
+                    days[key][1] += 1
+                    for i in range(len(tweetMoods)):
+                        days[key][0][i] += tweetMoods[i]
+                else:
+                    days[key] = [tweetMoods, 1.0]
 
-        # else:
-            # http found
-            # print "http found: " + unicode(tweet["text"]).encode('utf-8')
+            # else:
+                # http found
+                # print "http found: " + unicode(tweet["text"]).encode('utf-8')
 
-    outfile = open("output_results" + str(time.time()).replace(".","_") + ".csv","w")
-    line = "day,joy,trust,fear,surprise,sadness,disgust,anger,anticipation"
-    print line
-    outfile.writelines(line + "\n")
-    for key in days.keys():
-        sentiments, count = days[key]
-        line = key \
-              + "," + str(sentiments[0] / count) \
-              + "," + str(sentiments[1] / count) \
-              + "," + str(sentiments[2] / count) \
-              + "," + str(sentiments[3] / count) \
-              + "," + str(sentiments[4] / count) \
-              + "," + str(sentiments[5] / count) \
-              + "," + str(sentiments[6] / count) \
-              + "," + str(sentiments[7] / count)
+        outfile = open("output_results" + str(time.time()).replace(".","_") + ".csv","w")
+        line = "day,joy,trust,fear,surprise,sadness,disgust,anger,anticipation"
         print line
         outfile.writelines(line + "\n")
+        for key in days.keys():
+            sentiments, count = days[key]
+            line = key \
+                  + "," + str(sentiments[0] / count) \
+                  + "," + str(sentiments[1] / count) \
+                  + "," + str(sentiments[2] / count) \
+                  + "," + str(sentiments[3] / count) \
+                  + "," + str(sentiments[4] / count) \
+                  + "," + str(sentiments[5] / count) \
+                  + "," + str(sentiments[6] / count) \
+                  + "," + str(sentiments[7] / count)
+            print line
+            outfile.writelines(line + "\n")
 
-    # normalize: (Xi - min(X)) / (max(X) - min(X))
-    avg = []
-    for key in days.keys():
-        sentiments, count = days[key]
-        avg.append((key, [(sentiments[0] / count), (sentiments[1] / count), (sentiments[2] / count), (sentiments[3] / count), (sentiments[4] / count), (sentiments[5] / count), (sentiments[6] / count), (sentiments[7] / count)]))
-    max = 0.0
-    min = 10000.0
-    for day in avg:
-        for emotion in day[1]:
-            if emotion > max:
-                max = emotion
-            if emotion < min:
-                min = emotion
+        # normalize: (Xi - min(X)) / (max(X) - min(X))
+        avg = []
+        for key in days.keys():
+            sentiments, count = days[key]
+            avg.append((key, [(sentiments[0] / count), (sentiments[1] / count), (sentiments[2] / count), (sentiments[3] / count), (sentiments[4] / count), (sentiments[5] / count), (sentiments[6] / count), (sentiments[7] / count)]))
+        max = 0.0
+        min = 10000.0
+        for day in avg:
+            for emotion in day[1]:
+                if emotion > max:
+                    max = emotion
+                if emotion < min:
+                    min = emotion
 
-    # min and max calculated
-    for i in range(len(avg)):
-        for j in range(len(avg[i][1])):
-            avg[i][1][j] = (avg[i][1][j] - min) / (max - min)
-    line = "day,joy,trust,fear,surprise,sadness,disgust,anger,anticipation"
-    outfile.writelines(line + "\n")
-    for day in avg:
-        line = day[0] \
-              + "," + str(day[1][0]) \
-              + "," + str(day[1][1]) \
-              + "," + str(day[1][2]) \
-              + "," + str(day[1][3]) \
-              + "," + str(day[1][4]) \
-              + "," + str(day[1][5]) \
-              + "," + str(day[1][6]) \
-              + "," + str(day[1][7])
-        print line
+        # min and max calculated
+        for i in range(len(avg)):
+            for j in range(len(avg[i][1])):
+                avg[i][1][j] = (avg[i][1][j] - min) / (max - min)
+        line = "day,joy,trust,fear,surprise,sadness,disgust,anger,anticipation"
         outfile.writelines(line + "\n")
-    outfile.close()
+        for day in avg:
+            line = day[0] \
+                  + "," + str(day[1][0]) \
+                  + "," + str(day[1][1]) \
+                  + "," + str(day[1][2]) \
+                  + "," + str(day[1][3]) \
+                  + "," + str(day[1][4]) \
+                  + "," + str(day[1][5]) \
+                  + "," + str(day[1][6]) \
+                  + "," + str(day[1][7])
+            print line
+            outfile.writelines(line + "\n")
+        outfile.close()
+
+    except KeyboardInterrupt:
+        global finish
+        finish = True
 
 
 # def countEmotWords():
