9fcb242035ebb96b85b78bfcfaabe318c945e8f0
Michiel Fortuin <michiel.fortuin@gmail.com>
2014-12-11 14:32:44 +0100

    Added sentiSorted.csv for test purpose Use machine learning to predict the last 7 days of the month in the test data

--- code/ml/learning.py
+++ code/ml/learning.py
@@ -4,29 +4,53 @@ import networkx as NX
 import pylab
 import csv
 
+def converString(x): return float(x)
+
 data = []
 
-with open('..\\..\\..\\sentiSorted.csv', 'rb') as csvfile:
+#how many days in the past has to be learned
+learnDaysBack = 7
+#on how many days has the neural network to be tested
+learnData = -7
+
+#read data
+with open('..\\..\\data\\sentiSorted.csv', 'rb') as csvfile:
     rawData = csv.reader(csvfile, delimiter=';')
     rawData.next()
     for row in rawData:
         data.append(row[1:])
 
+#convert data so it can be pushed in to the neural network
 trainingSetInput = []
 trainingSetResult = []
-for i in range(7, len(data)-1):
+for i in range(learnDaysBack, len(data)+learnData):
     inputSet = []
-    for j in range(0, 7):
-        inputSet.extend(data[i-7+j])
-    trainingSetInput.append(inputSet)
-    trainingSetResult.append([data[i][-1]])
+    for j in range(0, learnDaysBack):
+        inputSet.extend(data[i-learnDaysBack+j])
+    trainingSetInput.append(map(converString, inputSet))
+    trainingSetResult.append([converString(data[i][-1])])
 
-for dataLine in trainingSetResult:
-    print dataLine
-
-conec = mlgraph((63,40,20,1))
+#create and train neural network
+conec = mlgraph((learnDaysBack*9,30,15,1))
 net = ffnet(conec)
-#net.train_genetic()
-#NX.draw_graphviz(net.graph, prog='dot')
+net.train_genetic(trainingSetInput[:learnData], trainingSetResult[:learnData])
+
+
+#test the neural network
+summationDifference = 0
+summationCount = 0
+maxDifference = 0
+
+testDataInput = trainingSetInput[learnData:]
+testDataActual = trainingSetResult[learnData:]
+for i in range(0, len(testDataInput)):
+    result = net.call(testDataInput[i])
+    difference = abs(testDataActual[i]/result - 1)
+    print "actual: " + str(testDataActual[i]) + " | NN: " + str(result) + " | difference: " + str(difference)
+    summationCount += 1
+    summationDifference += abs(testDataActual[i]/result - 1)
+    maxDifference = max(difference, maxDifference)
+
+print "Average difference: " + str(summationDifference/summationCount) + " | maximal difference: " + str(maxDifference)
 #NX.draw(net.graph)
 #pylab.show()
