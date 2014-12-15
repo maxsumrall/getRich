8bcd8c914757b509890d592b3c3a40abcd177dee
Max Sumrall <jmsumrall@gmail.com>
2014-12-15 14:04:00 +0100

    Changed insert call to work on list of tweets instead of every single tweet. This should be faster.

--- code/extractAndProcess.py
+++ code/extractAndProcess.py
@@ -3,9 +3,9 @@
 import os, tarfile, bz2, json, subprocess, pymongo
 
 tempdir = "/root/tempdir/"
-archive = "archive-09.tar"
+archive = "archive-08.tar"
 bz = []
-tar = tarfile.TarFile("archive-09.tar")
+tar = tarfile.TarFile("archive-08.tar")
 
 client = pymongo.MongoClient()
 db = client.test_database
@@ -13,13 +13,13 @@ tweets = db.tweets
 
 for member in tar:
     tar.extractall(path=tempdir, members=[member])
-    if (member.isfile()):
+    tweets_in_file = [] #for batch insertion
+    if (member.isfile()): #these things will either be files or the folders
         bz = bz2.BZ2File(tempdir + member.name)
         for line in bz:
             data = json.loads(line)
             if ("text" in data.keys()) and ("en" in data["lang"]):
-                # print data["text"]
-                tweets.insert({"created_at": data["created_at"], "text": data["text"]})
-        subprocess.check_output("rm " + tempdir + member.name, shell=True)
-        # if "n" in raw_input():
-        #       exit()
+                tweets_in_file.append({"created_at": data["created_at"], "text": data["text"]})
+        tweets.insert(tweets_in_file) #batch insert the tweets into mongo
+        subprocess.check_output("rm " + tempdir + member.name, shell=True) #Delete the file
+
