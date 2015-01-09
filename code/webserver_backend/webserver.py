f1c4c69d54373ed2543322ad47592daef6498344
Max Sumrall <jmsumrall@gmail.com>
2015-01-09 16:06:46 +0100

    backend for hosting results to frontend

--- code/webserver_backend/webserver.py
+++ code/webserver_backend/webserver.py
@@ -28,7 +28,7 @@ def convert_keys_to_string(dictionary):
     """Recursively converts dictionary keys to strings."""
     if not isinstance(dictionary, dict):
         return dictionary
-    return dict((str(k), convert_keys_to_string(v))
+    return dict((str(k).replace("\'", "\""), convert_keys_to_string(v))
         for k, v in dictionary.items())
 
 class index:
