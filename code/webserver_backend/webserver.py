cf127d21f7561e5772272345ee93905251955eb2
Max Sumrall <jmsumrall@gmail.com>
2015-01-09 13:44:20 +0100

    backend for hosting results to frontend

---
+++ code/webserver_backend/webserver.py
@@ -0,0 +1,35 @@
+__author__ = 'max'
+
+import web
+import json
+urls = (
+    '/', 'index',
+    "/data", "data"
+)
+
+data_file_name = "data.json"
+data_file = open(data_file_name,'r')
+data = json.load(data_file)
+data_file.close()
+
+
+
+class index:
+    def GET(self):
+        web.header('Access-Control-Allow-Origin',      '*')
+        return "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+
+class data:
+    def GET(self):
+        web.header('Access-Control-Allow-Origin',      '*')
+        return json.dumps(data)
+
+
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
+
+
+
