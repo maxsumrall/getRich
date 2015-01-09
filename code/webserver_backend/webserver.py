25d11e65d65820cb790ed09433f22649eafbd39b
Max Sumrall <jmsumrall@gmail.com>
2015-01-09 15:20:35 +0100

    backend for hosting results to frontend

--- code/webserver_backend/webserver.py
+++ code/webserver_backend/webserver.py
@@ -2,6 +2,16 @@ __author__ = 'max'
 
 import web
 import json
+import pymongo
+import datetime
+
+client = pymongo.MongoClient('mongodb://localhost:27017/')
+db = client.test_database
+col = db.results
+#today = datetime.today()
+#delta = datetime.timedelta(days=-30)
+
+
 urls = (
     '/', 'index',
     "/data", "data"
@@ -24,7 +34,19 @@ class index:
 class data:
     def GET(self):
         web.header('Access-Control-Allow-Origin',      '*')
-        return json.dumps(json_data)
+        data = []
+        for i in range(30):
+            date = (datetime.today - datetime.timedelta(days=-i))
+            date_key = str(date.day) + "/" + str(date.month) + "/" + str(date.year)
+            print date_key
+            try:
+                data.append(col.find({'x':date_key}).next())
+            except:
+                pass
+
+        return data
+
+
 
 
 
