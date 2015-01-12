13b67df6f9b828222477e51cfd600eebdab3d574
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-01-12 17:09:20 +0100

    Put everything in a single program tweets are processed faster by using state

--- code/processTweetsSparky.py
+++ code/processTweetsSparky.py
@@ -6,6 +6,7 @@ import plutchik
 import pymongo
 import time
 import os
+import processTweetsSparkyStream
 
 def dateTimeAndPlutchik(tweet):
     try:
@@ -17,13 +18,6 @@ def dateTimeAndPlutchik(tweet):
         # print "not a date"
         return "none", [0, 0, 0, 0, 0, 0, 0, 0]
 
-
-def retrieve(t):
-    client = pymongo.MongoClient('giedomak.nl')
-    db = client.test_database
-    tweets = db.tweets
-    return tweets.find_one(None, None, t)
-
 def split_tweet(t):
     result = t.split(';',1)
     result.extend([""])
@@ -33,86 +27,18 @@ def add_count_number(t):
     t[1].append(1)
     return t
 
-
-logFile = "C:\spark-1.2.0-bin-hadoop2.4/README.md"  # Should be some file on your system
-sc = SparkContext("local", "GetRich")
-ssc = StreamingContext(sc, 1)
-ssc.checkpoint("checkpoint")
-
-stream = ssc.socketTextStream("localhost", 9999)
-tweetData = stream.flatMap(lambda line: line.split("\r\n"))
-# tweetData.pprint()
-
-#tweetData = sc.textFile(logFile).cache()
-# tweetData = sc.parallelize([
-#         {"created_at": "Mon Jan 05 01:05:03 +0000 2010", "text":"http://"},
-#         {"created_at": "Mon Jan 06 01:05:03 +0000 2010", "text":"blub RT"},
-#         {"created_at": "Mon Jan 08 01:05:03 +0000 2010", "text":"Guido RT zit naast mij"},
-#         {"created_at": "Mon Jan 08 01:05:03 +0000 2010", "text":"blubsa"},
-#         {"created_at": "Mon Jan 08 01:05:03 +0000 2010", "text":"RT Ik heb lol"},
-#         {"created_at": "Mon Jan 08 01:05:03 +0000 2010", "text":"RT Ik heb lol en boos"},
-#         {"created_at": "Mon Jan 08 01:05:03 +0000 2010", "text":"RT Haat klote kut"},
-#     ])
-
-# client = pymongo.MongoClient('giedomak.nl')
-# db = client.test_database
-# tweets = db.tweets
-
-# tweetdata = range(100)
-# tweetdata = sc.parallelize(tweetdata)
-
-#tweetdata = tweetdata.map(lambda t: tweets.find_one(None, None, t))
-# tweetdata = tweetdata.map(retrieve)
-
-# print tweetdata.collect()
-
-tweetData = tweetData.map(split_tweet)
-# tweetData.pprint()
-# tweetData = tweetData.map(lambda s: (s[0], ''.join(str(x) for x in s[1:])))
-# tweetData.pprint()
-tweetData = tweetData.filter(lambda t: not("http" in t[1]) and "RT" in t[1])
-# tweetData = tweetData.filter(lambda t: not("http" in t[1]) and "RT" in t[1])
-# tweetData = tweetData.filter(lambda t: "RT" in t[1:])
-# tweetData.pprint()
-
-moodData = tweetData.map(dateTimeAndPlutchik)
-# moodData.pprint()
-moodData = moodData.filter(lambda t: not all(m == 0 for m in t[1]))
-# moodData.pprint()
-# print moodData.pprint()
-
-# moodData.pprint()
-
-moodData = moodData.map(add_count_number)
-moodData.checkpoint(15)
-
-# moodTotal = moodData.reduceByKey(lambda a, b: map(add, a, b))
-# moodTotal = moodData.reduceByKeyAndWindow(lambda a, b: map(add, a, b), lambda a, b: a, 3, 2)
-# moodTotal = moodData.reduceByKeyAndWindow(lambda a, b: map(add, a, b), None, 60*60)
-# moodTotal = moodData.reduceByKeyAndWindow(lambda a, b: map(add, a, b), None, 7*60*60)
-# moodCount = moodData.countByKey()     # No count in streaming API
-
 def addToState(newMood, runningTotal):
     if(runningTotal is None):
-        print newMood
-        return newMood
+        return reduce(lambda a, b: (map(add, a, b)), newMood, [0, 0, 0, 0, 0, 0, 0, 0, 0])
     else:
-        print map(add, newMood, runningTotal)
-        return map(add, newMood, runningTotal)
+        return map(add, reduce(lambda a, b: (map(add, a, b)), newMood, [0, 0, 0, 0, 0, 0, 0, 0, 0]), runningTotal)
 
-moodTotal = moodData.updateStateByKey(addToState)
-
-# moodDay = map(lambda a: (a[0], map(lambda b: b/moodCount[a[0]], a[1])), moodTotal.pprint())
-
-#moodTotal.saveAsTextFiles("test")
-
-# joy,trust,fear,surprise, sadness,disgust,anger,anticipation
 def makeJson(line):
+    # joy,trust,fear,surprise, sadness,disgust,anger,anticipation
     return {'x': line[0], '_id':line[0], 'joy':line[1][0]/line[1][8], 'trust':line[1][1]/line[1][8], 'fear':line[1][2]/line[1][8],
     'surprise':line[1][3]/line[1][8], 'sadness':line[1][4]/line[1][8], 'disgust':line[1][5]/line[1][8],
     'anger':line[1][6]/line[1][8], 'anticipation':line[1][7]/line[1][8],
     'prediction':0, 'Stock':0}
-
 
 def processResults(rdd):
     print rdd.collect()
@@ -132,14 +58,38 @@ def processResults(rdd):
             print result
             col.save(result)
 
+logFile = "C:\spark-1.2.0-bin-hadoop2.4/README.md"  # Should be some file on your system
+sc = SparkContext("local[4]", "GetRich")
+ssc = StreamingContext(sc, 1)
+ssc.checkpoint("checkpoint")
+
+stream = ssc.socketTextStream("localhost", 9998)
+tweetData = stream.flatMap(lambda line: line.split("\r\n"))
+
+tweetData = tweetData.map(split_tweet)
+#totalTweetsPerDay = tweetData.map(lambda a: (0, 1)).updateStateByKey(lambda a, running: (sum(a)+(running or 0)))
+
+
+tweetData = tweetData.filter(lambda t: not("http" in t[1]) and "RT" in t[1])
+
+moodData = tweetData.map(dateTimeAndPlutchik)
+moodData = moodData.filter(lambda t: not all(m == 0 for m in t[1]))
+moodData = moodData.map(add_count_number)
+moodData.checkpoint(15)
+
+moodTotal = moodData.reduceByKey(lambda a, b: map(add, a, b)).updateStateByKey(addToState)
+
 moodTotal.foreachRDD(processResults)
 
 moodTotal.pprint()
-# moodCount.pprint()
-# moodDay.pprint()
+
+#def printValue(a):
+#    print a.collect()
+#totalTweetsPerDay.foreachRDD(printValue)
 
 ssc.start()             # Start the computation
 #raw_input('Press enter to close')
-#time.sleep(10)
-ssc.awaitTermination()  # Wait for the computation to terminate
-#ssc.stop(stopSparkContext=True, stopGraceFully=True)
+#time.sleep(30)
+#ssc.awaitTermination()  # Wait for the computation to terminate
+processTweetsSparkyStream.SendTweets()
+ssc.stop(stopSparkContext=True, stopGraceFully=True)
