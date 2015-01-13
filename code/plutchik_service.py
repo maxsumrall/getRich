28d8fe1b0f27f14a95da69400710474f67feae94
Max Sumrall <jmsumrall@gmail.com>
2015-01-13 15:12:04 +0100

    backend which gives you plutchik stuff

--- code/plutchik_service.py
+++ code/plutchik_service.py
@@ -1,1 +1,38 @@
 __author__ = 'max'
+
+import web
+import json
+from datetime import *
+import plutchik
+
+
+
+urls = (
+    '/', 'index',
+    "/plutchik", "getsentiment"
+)
+
+
+class index:
+    def GET(self):
+        web.header('Access-Control-Allow-Origin',      '*')
+        return "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+
+class getsentiment:
+    def GET(self):
+        web.header('Access-Control-Allow-Origin',      '*')
+
+
+        return plutchik.executeTweet(web.input()["text"])
+
+
+
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
+
+
+
+
